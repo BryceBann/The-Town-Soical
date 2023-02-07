6e2f1f60c7623b8fd8602ffe022947ee8795f420
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-02-07 14:42:31 -0600

    added modal to delete user as saftey feature

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Home/Home.css";
 import NavBar from "./../Navbar/Navbar";
 import { QUERY_ME } from "../../utils/queries";
 import { useQuery, useMutation } from "@apollo/client";
 import PostsList from "../Posts/Posts";
 import { DELETE_USER } from "../../utils/mutations";
-import { AiFillDelete } from "react-icons/ai"
-import { NavLink } from "react-bootstrap";
+import { AiFillDelete } from "react-icons/ai";
+import { NavLink, Modal, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import Auth from "../../utils/auth";
 
 const Profile = (refetch) => {
-  const { loading, data } = useQuery(QUERY_ME)
+  const { loading, data } = useQuery(QUERY_ME);
   const currentUsername = data?.me?.username;
   const [deleteUser] = useMutation(DELETE_USER);
+  const [showModal, setShowModal] = useState(false);
 
   const user = data?.me || {};
   const userId = data?.me._id || {};
@@ -45,29 +46,49 @@ const Profile = (refetch) => {
   }
 
   return (
-    <div className="Home">
-      <NavBar />
-      <div className="postCard">
-        <div className="profiledelete">
-        <p className="deleteusers"> Delete Profile</p>
-        <NavLink
-        as={Link}
-        to="/"
-        onClick={Auth.logout}
-        className="DeleteUser"
-        > 
-          <AiFillDelete onSubmit={Auth.logout} onClick={() => handleDelete(user._id)} />
-        </NavLink>
+    <>
+      <div className="Home">
+        <NavBar />
+        <div className="postCard">
+          <div className="profiledelete">
+            <p className="deleteusers"> Delete Profile <AiFillDelete className="DeleteBtn" onClick={() => setShowModal(true)} /></p>
+            
+          </div>
+          <PostsList
+            posts={user.posted}
+            currentUsername={currentUsername}
+            showTitle={false}
+            showUsername={false}
+            refetch={refetch}
+          />
         </div>
-        <PostsList
-          posts={user.posted}
-          currentUsername={currentUsername}
-          showTitle={false}
-          showUsername={false}
-          refetch={refetch}
-        />
       </div>
-    </div>
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Delete Profile</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>Are you sure you want to delete your profile?</p>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={() => setShowModal(false)} variant="secondary">
+              No
+            </Button>
+            <NavLink as={Link} to="/" onClick={Auth.logout}>
+            <Button
+              onSubmit={Auth.logout}
+              onClick={() => handleDelete(user._id)}
+              variant="primary"
+            >
+              Yes
+            </Button>
+
+            </NavLink>
+          </Modal.Footer>
+        </Modal>
+    </>
   );
 };
 export default Profile;
